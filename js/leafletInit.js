d0581cb9abceb89e27057265145eac7ceabbe498
stolbov <stolbov@informatica.ru>
2014-12-05 17:27:29 +0300

    Значки!!!

--- js/leafletInit.js
+++ js/leafletInit.js
@@ -16,28 +16,27 @@ var greenIcon = L.icon({
     shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
-//var marker = L.marker([56.123578,47.239479], {icon: greenIcon}).addTo(map);
-// var marker = L.marker([56.123578,47.239479]).addTo(map);
-var iconO = ['leaf-orange', 'leaf-red', 'leaf-green'];
-for(var i = 0; i < 100; i++){
+
+var markerColors = ['redMarker', 'yellowMarker', 'blueMarker'];
+var markerIcons = ['auto', 'star', 'snow'];
+for(var i = 0; i < 1000; i++){
 	var rndX = Math.random() * (57 - 54) + 54,
 		rndY = Math.random() * (48 - 46) + 46;
-	var iconNum = Math.random() * (2);
-	/*
-	var Icon = L.icon({
-	    iconUrl: 'http://leafletjs.com/docs/images/' + iconO[iconNum.toFixed()] + '.png',
-	    shadowUrl: 'http://leafletjs.com/docs/images/leaf-shadow.png',
+	var markerColorNum = Math.floor(Math.random() * (3));
+	var markerIconNum = Math.floor(Math.random() * (3));
 
-	    iconSize:     [38, 95], // size of the icon
-	    shadowSize:   [50, 64], // size of the shadow
-	    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-	    shadowAnchor: [4, 62],  // the same for the shadow
-	    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-	});
-	*/
 	var Icon = L.divIcon({
-		className: 'my-div-icon',
-		html: '+'
+		className: 'leafletIconWr ' + markerColors[markerColorNum],
+		iconSize: null,
+		html: '<div class="'  + markerIcons[markerIconNum] + 'Icon"></div>'
 	});
-	L.marker([rndX.toFixed(4),rndY.toFixed(4)], {icon: Icon}).bindPopup("I am a happy yellow marker )...").addTo(map); 
+	L.marker([rndX.toFixed(4),rndY.toFixed(4)], {icon: Icon}).bindPopup("I am a happy " + markerIcons[markerIconNum] + " " + markerColors[markerColorNum]  + " :)...").addTo(map); 
 }
+/*
+var Icon = L.divIcon({
+		className: 'leafletIconWr blueMarker',
+		iconSize: null,
+		html: '<div class="snowIcon"></div>'
+	});
+L.marker([56.123578,47.239479], {icon: Icon}).bindPopup("I am a happy marker :)...").addTo(map);
+*/
